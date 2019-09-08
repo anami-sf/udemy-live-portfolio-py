a2fa973438598ca2baa4c40521b5b9abffa23cb5
anami823 <anami823@gmail.com>
2019-09-07 22:20:12 -0700

    Add Job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+#The code to create models is the same regardless of the db chosen for the project
+
+# With models.Model django will save the model onto the db
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+
+
